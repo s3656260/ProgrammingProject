5bb06862113ae293dc3d81789f437899b1e80ea6
GamesWorth <s3691198@student.rmit.edu.au>
2019-08-14 21:45:20 +1000

    elaborated structure of backend again

--- sparkjava/src/main/java/app.java
+++ sparkjava/src/main/java/app.java
@@ -1,3 +1,13 @@
+import org.apache.log4j.BasicConfigurator;
+import view.webService;
 
 public class app {
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        webService test = new webService("test",app::sayHello);
+        test.startService();
+    }
+    public static String sayHello(){
+        return "hello";
+    }
 }
--- sparkjava/src/main/java/view/webService.java
+++ sparkjava/src/main/java/view/webService.java
@@ -1,4 +1,18 @@
 package view;
-
+import static spark.Spark.*;
 public class webService {
+
+    private String _serviceName;
+    private Runnable _serviceAction;
+
+    public webService(String serviceName, Runnable serviceAction){
+        _serviceAction = serviceAction;
+        _serviceName = serviceName;
+    }
+
+    public void startService(){
+        String pathStr = "/"+_serviceName;
+        get(pathStr, (req, res) -> _serviceAction);
+        //post(pathStr, (request, response) ->{});
+     }
 }
