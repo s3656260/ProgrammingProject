a59576892ab0d7851f73f98a562d567c7aefae0f
GamesWorth <s3691198@student.rmit.edu.au>
2019-09-17 16:52:53 +1000

    Generating some local files to speed webservice

--- sparkjava/src/main/java/app.java
+++ sparkjava/src/main/java/app.java
@@ -1,20 +1,37 @@
-import controller.apiService;
+import controller.jsonService;
 import org.apache.log4j.BasicConfigurator;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import view.webService;
-
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class app {
     public static void main(String[] args) throws IOException {
         BasicConfigurator.configure();
-        //apiService foo = new apiService();
-        //foo.genList();
-        liveService();
+        //liveService();
+        makeJson();
+    }
+    private static void makeJson(){
+        jsonService jService = new jsonService("database/companyNames.json");
+        String str = jService.getAsString();
+        JSONArray jArr = new JSONArray(str);
+
+        JSONObject jOb = new JSONObject();
+        for (int i=0; i < jArr.length(); i++) {
+            JSONObject x = jArr.getJSONObject(i);
+            String sym = x.getString("symbol");
+            String cName = x.getString("name");
+
+            jOb.put(sym,cName);
+        }
+        //String sr = jArr.getJSONObject(1).getString("name");
+        String sr = jOb.getString("A");
+        System.out.println(sr);
     }
     private static void liveService(){
         webService test = new webService("test","foo");
         test.startService();
     }
+
 }
--- sparkjava/src/main/java/controller/databaseService.java
+++ sparkjava/src/main/java/controller/databaseService.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 
 public class databaseService {
     public databaseService(){
-        Connection c =;
+
     }
 
 }
